5be89756236ba13218572b4fd2a252c704e62d44
Rishabh Singh <ris8645208@gmail.com>
2021-08-12 21:03:01 +0530

    Outline removed from inputs learning TypeScript on the way

---
+++ sandbox.js
@@ -0,0 +1,3 @@
+var names = ["luigi", "mario", "yoshi"];
+names.push("name");
+var mixed = ["kent", 20, "chum-lee", 400, false];
---
+++ sandbox.ts
@@ -0,0 +1,5 @@
+let names = ["luigi", "mario" , "yoshi"];
+
+names.push("name");
+
+let mixed = ["kent",20,"chum-lee",400, false];
